895ddd1d08d5b6ffdd9aaa2df527f23a68cdbf9a
eb <lizzy.kayballantine@gmail.com>
2021-07-20 10:59:34 +0100

    correct a test, now all passing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,9 @@
 def fizzbuzz(number)
-  case number
-  when number % 3 == 0 && number % 5 == 0
+  if number % 3 == 0 && number % 5 == 0
     "fizzbuzz"
-  when number % 3 == 0
+  elsif number % 3 == 0 
     "fizz"
-  when number % 5 == 0 
+  elsif number % 5 == 0 
     "buzz"
   else
     number
